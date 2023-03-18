d253e7a3f7cb245343201fca350096104388518b
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-18 18:22:57 +0500

    imported dropzone

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
+// dropzone
+Dropzone.options.myGreatDropzone = { // camelized version of the `id`
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    accept: function(file, done) {
+      if (file.name == "justinbieber.jpg") {
+        done("Naha, you don't.");
+      }
+      else { done(); }
+    }
+  };
+
 
 canvas.style.height = '600px';
 canvas.style.width = '800px'
@@ -10,3 +22,5 @@ ctx.beginPath();
 ctx.moveTo(30, 30);
 ctx.lineTo(200, 100);
 ctx.stroke();
+
+
