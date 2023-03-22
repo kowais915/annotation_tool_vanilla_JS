3268662a10860bcff6ea17790e5ce0f2ea0fd0d3
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-22 05:13:47 +0500

    :sparkles: you can upload and display pictures on the canvas

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 
 const input = document.querySelector('#uploaded_image')
 const display = document.querySelector('#canvas')
-let image_up = "";
+const ctx = display.getContext('2d');
 
-display.innerHTML = `<p>Hi I am owais</p>`
+
 
 
 
@@ -16,11 +16,26 @@ display.innerHTML = `<p>Hi I am owais</p>`
 
 input.addEventListener('change', (event)=>{
 
+  const file = event.target.files[0];
+
   const reader = new FileReader();
-  reader.readAsDataURL(input.files[0])
-  reader.addEventListener('load', ()=>{
-    display.innerHTML = `<img src = '${reader.result}'/>`
+
+
+
+  reader.addEventListener('load', (event)=>{
+
+    const img = new Image();
+    img.onload = function () { 
+        display.width  = img.width;
+        display.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+     };
+
+     img.src = event.target.result;
+     
   })
 
+  reader.readAsDataURL(file)
     
 })
